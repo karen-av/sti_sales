4c28efce030818f14e10ed3398af183e9ffe0f79
Karen <ka.spk27@gmail.com>
2022-11-11 14:36:26 +0300

    1

--- test.py
+++ test.py
@@ -4,5 +4,5 @@ from werkzeug.security import check_password_hash, generate_password_hash
 name, branch, position, mail, status = 'KO', 'vv', 'admin', 'admin@asd.com', 'admin'
 user_password = 'edrfTgr34'
 hash = generate_password_hash(user_password, "pbkdf2:sha256")    
-print(hash)   
+print(f'h - {hash}')   
     
